e07ce0b6c5730e893be474b32a31b0c9f4e609e0
petruk1 <vpetruk.osn@gmail.com>
2018-09-09 13:58:51 +0300

    comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,16 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {AppComponent} from './app.component';
-import {AngularFireModule} from 'angularfire2';
-import {environment} from '../environments/environment';
-import {AngularFireAuthModule} from 'angularfire2/auth';
-import {RouterModule, Routes} from '@angular/router';
-import {ReactiveFormsModule} from '@angular/forms';
-import {AuthModule} from './auth/auth.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-const APP_ROUTES: Routes = [
-  {
-    path: '', redirectTo: 'auth', pathMatch: 'full'
-  }
-];
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule,
-    ReactiveFormsModule,
-    AuthModule,
-    RouterModule.forRoot(APP_ROUTES)
-
+    BrowserModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
